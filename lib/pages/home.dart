270a1c98901c6b59a5c8c6a1095015f90d272307
Godopu <godopu16@gmail.com>
2021-04-25 13:19:53 +0900

    commit 2021-04-25]13:19:48

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,73 +13,102 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  ScrollController _controller = ScrollController();
+  final ScrollController _controller = ScrollController();
+  Timer? _timer = null;
+  double _dragStart = 0;
+  double _dragEnd = 0;
   double _height = 0;
-  double _cur = 0;
+  int _cur = 0;
   bool _doingAnimation = false;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+
+  startAnimation(bool isDown) {
+    if (isDown && _cur < 3) {
+      _cur++;
+      _controller.animateTo(_cur * _height,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
+    } else if (!isDown && _cur > 0) {
+      _cur--;
+      _controller.animateTo(_cur * _height,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Hello world");
     _height = MediaQuery.of(context).size.height;
 
+    if (_cur != 0) {
+      // print("^^");
+      if (_timer != null && _timer!.isActive) _timer!.cancel();
+
+      _timer = Timer(Duration(milliseconds: 100),
+          () => _controller.jumpTo(_cur * _height));
+    }
+
+    //
     return Scaffold(
-      body: Listener(
-        onPointerSignal: (pointerSignal) {
-          if (pointerSignal is PointerScrollEvent && !_doingAnimation) {
-            _doingAnimation = true;
-            Timer(Duration(milliseconds: 500), () => _doingAnimation = false);
-            if (pointerSignal.scrollDelta.dy > 0) {
-              if (_cur < 3 * _height) {
-                _cur += _height;
-                if (_cur % _height != 0) {
-                  _cur += (_height - (_cur % _height));
+      body: Stack(
+        children: [
+          Listener(
+            onPointerSignal: (pointerSignal) {
+              if (pointerSignal is PointerScrollEvent && !_doingAnimation) {
+                _doingAnimation = true;
+                Timer(
+                    Duration(milliseconds: 500), () => _doingAnimation = false);
+                if (pointerSignal.scrollDelta.dy > 0) {
+                  startAnimation(true);
+                } else {
+                  startAnimation(false);
                 }
               }
-
-              _controller.animateTo(_cur,
-                  duration: Duration(milliseconds: 500), curve: Curves.ease);
-            } else {
-              if (_cur >= _height) {
-                _cur -= _height;
-                if (_cur % _height != 0) {
-                  _cur -= _cur % _height;
+            },
+            child: GestureDetector(
+              onVerticalDragDown: (details) {
+                _dragStart = details.localPosition.dy;
+                _dragEnd = details.localPosition.dy;
+              },
+              onVerticalDragUpdate: (details) {
+                _dragEnd = details.localPosition.dy;
+              },
+              onVerticalDragEnd: (details) {
+                if (_dragStart > _dragEnd) {
+                  startAnimation(true);
+                } else {
+                  startAnimation(false);
                 }
-              } else if (_cur > 0) _cur = 0;
-              _controller.animateTo(_cur,
-                  duration: Duration(milliseconds: 500), curve: Curves.ease);
-            }
-          }
-        },
-        child: SingleChildScrollView(
-          physics: NeverScrollableScrollPhysics(),
-          controller: _controller,
-          child: Column(
-            children: [
-              Container(
-                color: Colors.red,
-                height: _height,
+              },
+              child: SingleChildScrollView(
+                physics: NeverScrollableScrollPhysics(),
+                controller: _controller,
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      height: _height,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      height: _height,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      height: _height,
+                    ),
+                    Container(
+                      color: Colors.black,
+                      height: _height,
+                    ),
+                  ],
+                ),
               ),
-              Container(
-                color: Colors.green,
-                height: _height,
-              ),
-              Container(
-                color: Colors.blue,
-                height: _height,
-              ),
-              Container(
-                color: Colors.black,
-                height: _height,
-              ),
-            ],
+            ),
           ),
-        ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            color: Colors.grey,
+          ),
+        ],
       ),
     );
   }
